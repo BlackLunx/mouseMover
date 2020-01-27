cda2edd9bd3604652a9d8d0dce06bce723900b8a
Mikhail Zhelezin <mihonblack@gmail.com>
2020-01-27 17:40:46 +0300

    ver 1.01b

--- Checker.cpp
+++ Checker.cpp
@@ -7,13 +7,14 @@ int main()
 {
 	srand(time(0));
 	int end = 0, was = 0, seconds;
-	while (!end) {
+	while (true) {
 		if (GetAsyncKeyState(VK_HOME)) //КНОПКА HOME - начало
-			was++;
+			was = 1;
 		if (GetAsyncKeyState(VK_END))  // КНОПКА END - конец
-			end++;
+			was = 0;
 		if (was) {
 			mouse_event(MOUSEEVENTF_MOVE, rand() % 200 - 100, rand() % 200 - 100, 0, 0);
+			mouse_event(MOUSEEVENTF_WHEEL, rand() % 200 - 100, 0, 0, 0);
 			keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
 // Simulate a key release
 			keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
